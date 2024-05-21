629bb17df4611cefadddbf9014facb9481fb119b
WaysideCandy <l18200941@pachuca.tecnm.mx>
2024-05-21 11:55:10 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -28,7 +28,7 @@ function callApiRequest() {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "http://3.11.80.35/iot-car-control/back-end/apis/getRegistro.php"
+      "https://3.11.80.35/iot-car-control/back-end/apis/getRegistro.php"
     )
     .then(function (response) {
       // manejar respuesta exitosa
